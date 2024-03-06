3639f3786cc51c999809f5506861eb6d674f4696
ItamarPeleg <pelegitamar2@gmail.com>
2024-03-06 23:53:21 +0200

    Add New Design

--- WebApp/steps/src/pages/NewHome.js
+++ WebApp/steps/src/pages/NewHome.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import BentoGrid from "@bentogrid/core";
 import downArrow from '../assets/down-arrow-svgrepo-com.svg';
+import newProject from '../assets/create-folder-svgrepo-com.svg';
 import { useNavigate } from 'react-router-dom';
 import { useWallet } from '../utils/WalletContext';
 
@@ -8,6 +9,7 @@ const NewHome = () => {
     const [lastProjects, setLastProjects]  = useState({lastProjects: [], localProjects: []})
     const navigate = useNavigate()
     const { contract } = useWallet();
+    const [hoverNewProject, setHoverNewProject] = useState(false);
 
     useEffect(() => {
         const myBento = new BentoGrid({
@@ -43,19 +45,25 @@ const NewHome = () => {
         <div className='backgroundNew center'>
             <div className='boxes'>
                 <div class="bentogrid">
-                    <div className='box boxCreateNewProject' data-bento="1x1">
-                        <h1 className='boxTitle'>Create <br /> New Project</h1>
+                    <div className='box boxCreateNewProject' data-bento="1x1" onMouseLeave={() => setHoverNewProject(false)} onMouseEnter={() => setHoverNewProject(true)}>
+                        {!hoverNewProject ? 
+                            <div className='boxBox-newProject'>
+                                <img src={newProject} alt="" />
+                            </div> 
+                            : 
+                            <h1 className='BoxboxTitle'>Create <br /> New Project</h1>
+                        }
                     </div>
                     <div className='box boxBox-search' data-bento="2x1">
-                        <h1 className='Title'>Search</h1>
+                        <h1 className='boxTitle'>Search</h1>
                         <input className='search' type="text"/>
                     </div>
                     <div className='box' data-bento="2x1"></div>
                     <div className='box' data-bento="1x2">
-                        <div className='box-projects'>
-                            <h1 className='Title'>Recent activity</h1>
+                        <div className='Boxbox-projects'>
+                            <h1 className='boxTitle'>Recent activity</h1>
                             {lastProjects.localProjects.map((item, index) => (
-                            <div onClick={() => {navigate(`project/${item}`)}} key={index} whileHover={{scale: 1.05}} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ type: "spring", duration: 0.7 }} className='project-box-load'>
+                            <div onClick={() => {navigate(`project/${item}`)}} key={index} whileHover={{scale: 1.05}} initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ type: "spring", duration: 0.7 }} className='BoxProject-box-load'>
                             <span className='project-box-header'>{item}</span>
                             <span>----------- | -----------</span>
                             </div>
